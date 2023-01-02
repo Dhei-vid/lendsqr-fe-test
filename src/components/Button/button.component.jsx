74a01fbe0f9889a715232a35badaeaffd2eca9ca
David Etuk <djetuk@gmail.com>
2023-01-02 14:12:37 +0100

    set up the password toggle effect and added the login image

--- src/components/Button/button.component.jsx
+++ src/components/Button/button.component.jsx
@@ -1,9 +1,7 @@
 import './button.styles.scss'
 
-const Button = ({ children }) => {
-  console.log(children)
-
-  return <button className='button-container'>{children}</button>
-}
+const Button = ({ children }) => (
+  <button className='button-container'>{children}</button>
+)
 
 export default Button
--- src/components/login/login.form.component.jsx
+++ src/components/login/login.form.component.jsx
@@ -1,19 +1,50 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import Button from '../Button/button.component'
 import './login.form.style.scss'
 
 const Login = () => {
+  //   const [formFields, setFormFields] = useState(defaultFormFields)
+  const [password, setPassword] = useState('')
+  const [showPassword, setShowPassword] = useState(false)
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    alert('Successfully submitted')
+  }
+
+  const onChangeHandler = e => {
+    setPassword(e.target.value)
+  }
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword)
+  }
+
   return (
     <div className='login-container'>
-      <h2> Welcome!</h2>
-      <p>Enter details to login.</p>
-      <form>
-        <input type='text' placeholder='Email' />
-        <input type='text' placeholder='Password' />
-        <span>Forgot password?</span>
+      <div className='left-half'></div>
 
-        <Button children={`Log in`} />
-      </form>
+      <div className='right-half'>
+        <h2> Welcome!</h2>
+        <p>Enter details to login.</p>
+        <form>
+          <input type='text' placeholder='Email' name='email' />
+          <div className='passwordField'>
+            <input
+              type={showPassword ? 'text' : 'password'}
+              name='password'
+              placeholder='Password'
+              onChange={onChangeHandler}
+            />
+            <span className='password-cta' onClick={toggleShowPassword}>
+              Show
+            </span>
+          </div>
+          <span>Forgot password?</span>
+          <Button children={`Log in`} onSubmit={handleSubmit} />
+        </form>
+      </div>
     </div>
   )
 }
