8c8f21a7ead5c78a61135147ce0200160ed1d370
David Etuk <djetuk@gmail.com>
2023-01-04 15:57:33 +0100

    created the dashboard

--- src/components/dashboard/dashboard.component.jsx
+++ src/components/dashboard/dashboard.component.jsx
@@ -1,3 +1,11 @@
-const Dashboard = () => {}
+import Users from '../users/users.component'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Users />
+    </div>
+  )
+}
 
 export default Dashboard
--- src/components/users/users.component.jsx
+++ src/components/users/users.component.jsx
@@ -3,46 +3,48 @@ import icon2 from '../../assets/icons/icon2.png'
 import icon3 from '../../assets/icons/icon3.png'
 import icon4 from '../../assets/icons/icon4.png'
 
+import './user.styles.scss'
+
 const Users = () => {
   return (
-    <div>
-      <div>
-        <ul>
+    <div className='user-container'>
+      <div className='user-card-container'>
+        <ul className='user-card-elements'>
           <li>
             <img src={icon1} alt='icon' />
           </li>
-          <li>USERS</li>
-          <li>2,453</li>
+          <li className='user-text'> USERS</li>
+          <li className='user-number'>2,453</li>
         </ul>
       </div>
 
-      <div>
-        <ul>
+      <div className='user-card-container'>
+        <ul className='user-card-elements'>
           <li>
             <img src={icon2} alt='icon' />
           </li>
-          <li>ACTIVE USERS</li>
-          <li>2,453</li>
+          <li className='user-text'>ACTIVE USERS</li>
+          <li className='user-number'>2,453</li>
         </ul>
       </div>
 
-      <div>
-        <ul>
+      <div className='user-card-container'>
+        <ul className='user-card-elements'>
           <li>
             <img src={icon3} alt='icon' />
           </li>
-          <li>USERS WITH LOANS</li>
-          <li>12,453</li>
+          <li className='user-text'>USERS WITH LOANS</li>
+          <li className='user-number'>12,453</li>
         </ul>
       </div>
 
-      <div>
-        <ul>
+      <div className='user-card-container'>
+        <ul className='user-card-elements'>
           <li>
             <img src={icon4} alt='icon' />
           </li>
-          <li>USERS WITH SAVINGS</li>
-          <li>102,453</li>
+          <li className='user-text'>USERS WITH SAVINGS</li>
+          <li className='user-number'>102,453</li>
         </ul>
       </div>
     </div>
