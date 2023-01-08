bcbd791a7ab800212ce903b7b003badac2700be4
David Etuk <djetuk@gmail.com>
2023-01-08 21:15:47 +0100

    adding the logout part to the side bar

--- src/components/logout/logout.component.jsx
+++ src/components/logout/logout.component.jsx
@@ -1,9 +1,18 @@
 import { FiLogOut } from 'react-icons/fi'
+import { FcSettings } from 'react-icons/fc'
+
 import './logout.styles.scss'
 
 const LogOut = () => {
   return (
     <div className='logout-container'>
+      <div className='side-bar-item side-bar-items-container'>
+        <span className='side-bar-icons'>
+          <FcSettings />
+        </span>
+        <span className='side-bar-title'> Systems Messages</span>
+      </div>
+
       <ul className='logout-list'>
         <li className='logout-icon-text logout-list-item'>
           <FiLogOut />
