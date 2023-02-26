9f533bb9b46aecc7dcd807290e86fc43e6251418
David Etuk <djetuk@gmail.com>
2023-02-26 17:05:15 +0100

    fix: removed unused imports

--- src/routes/dashboard/dashboard.component.jsx
+++ src/routes/dashboard/dashboard.component.jsx
@@ -1,26 +1,25 @@
-import Users from '../../components/users/users.component'
-import UserData from '../../components/userData/userData.components'
-import SideBar from '../../components/sideBar/sidebar.component'
-import Navigation from '../../components/navigation/navigation.component'
+import Users from "../../components/users/users.component";
+import UserData from "../../components/userData/userData.components";
+import SideBar from "../../components/sideBar/sidebar.component";
+import Navigation from "../../components/navigation/navigation.component";
 
-import './dashboard.styles.scss'
-import { RiFacebookBoxFill, RiFileCodeFill } from 'react-icons/ri'
+import "./dashboard.styles.scss";
 
 const Dashboard = () => {
   return (
-    <div className='dashboard-container'>
+    <div className="dashboard-container">
       <Navigation />
-      <div className='dashboard-info'>
-        <div className='sidebar'>
+      <div className="dashboard-info">
+        <div className="sidebar">
           <SideBar />
         </div>
-        <div className='user-info'>
+        <div className="user-info">
           <Users />
           <UserData />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
