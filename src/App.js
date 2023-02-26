a5d05e706b69dea95212163d4bed1439ba53be3f
David Etuk <djetuk@gmail.com>
2023-02-26 23:36:44 +0100

    fix: routed the default path to the login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 
-import Navigation from './components/navigation/navigation.component'
-import Login from './routes/login/login.form.component'
-import Dashboard from './routes/dashboard/dashboard.component'
-import UserDetails from './routes/userDetails/userDetails.components'
-import Users from './components/users/users.component'
-import Filter from './components/filter/filter.component'
-import Status from './components/status/status.component'
+import Login from "./routes/login/login.form.component";
+import Dashboard from "./routes/dashboard/dashboard.component";
+import UserDetails from "./routes/userDetails/userDetails.components";
 
-// testing
-import UserData from './components/userData/userData.components'
-
-function App () {
+function App() {
   return (
     <Routes>
-      <Route path='/' element={<Login />} />
-      <Route path='dashboard' element={<Dashboard />} />
-      <Route path='userdetails' element={<UserDetails />} />
+      <Route path="/" element={<Login />} />
+      <Route path="dashboard" element={<Dashboard />} />
+      <Route path="userdetails" element={<UserDetails />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+// dbConnect();
+export default App;
