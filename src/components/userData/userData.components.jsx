269e86b14040cd0b79d353f2b401a3c775de5475
David Etuk <djetuk@gmail.com>
2023-02-28 20:46:09 +0100

    fix: refactoring userDate and userDetails

--- src/components/userData/userData.components.jsx
+++ src/components/userData/userData.components.jsx
@@ -1,146 +1,146 @@
-import { useEffect, useState } from 'react'
-import dateFormat from 'dateformat'
+import { useEffect, useState } from "react";
+import dateFormat from "dateformat";
 
-import Pagination from '../pagination/pagination.component'
-import Filter from '../filter/filter.component'
-import Status from '../../components/status/status.component'
+import Pagination from "../pagination/pagination.component";
+import Filter from "../filter/filter.component";
+import Status from "../../components/status/status.component";
 
-import { BiFilter } from 'react-icons/bi'
-import { RxDotsVertical } from 'react-icons/rx'
+import { BiFilter } from "react-icons/bi";
+import { RxDotsVertical } from "react-icons/rx";
 
-import './userData.styles.scss'
+import "./userData.styles.scss";
 
 const UserData = () => {
-  const [data, setData] = useState(null)
-  const [filter, setFilter] = useState(false)
-  const [status, setStatus] = useState(false)
+  const [data, setData] = useState(null);
+  const [filter, setFilter] = useState(false);
+  const [status, setStatus] = useState(false);
 
   const filterHandler = () => {
-    setFilter(!filter)
+    setFilter(!filter);
 
-    console.log(filter)
-  }
+    console.log(filter);
+  };
 
   const statusHandler = () => {
-    setStatus(!status)
-  }
+    setStatus(!status);
+  };
 
   useEffect(() => {
-    async function fetchData () {
+    async function fetchData() {
       const response = await fetch(
-        'https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users'
-      )
-      const data = await response.json()
+        "https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users"
+      );
+      const data = await response.json();
 
       // storing data in local storage
-      localStorage.setItem('data', JSON.stringify(data))
+      localStorage.setItem("data", JSON.stringify(data));
 
       // retrieving data from local storage
-      const userData = JSON.parse(localStorage.getItem('data'))
+      const userData = JSON.parse(localStorage.getItem("data"));
 
       // console.log(userData)
-      setData(userData)
+      setData(userData);
     }
 
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
-  console.log(data)
+  console.log(data);
 
   return (
     <>
-      {status ? <Status /> : ''}
-      <div className='user-data-container'>
-        {filter ? <Filter /> : ''}
+      {status ? <Status /> : ""}
+      <div className="user-data-container">
+        {filter ? <Filter /> : ""}
         <table>
           <thead>
-            <tr className='data-heading-container'>
-              <th className='data-headings'>
+            <tr className="data-heading-container">
+              <th className="data-headings">
                 Organization
-                <span className='data-heading-icon'>
+                <span className="data-heading-icon">
                   <BiFilter
                     onClick={filterHandler}
-                    className='data-icon'
-                    size='20px'
+                    className="data-icon"
+                    size="20px"
                     style={{
-                      position: 'absolute',
+                      position: "absolute",
                       // top: '-3px',
-                      cursor: 'pointer'
+                      cursor: "pointer",
                     }}
                   />
                 </span>
               </th>
-              <th className='data-headings'>
+              <th className="data-headings">
                 Username
-                <span className='data-heading-icon'>
+                <span className="data-heading-icon">
                   <BiFilter
                     onClick={filterHandler}
-                    className='data-icon'
-                    size='20px'
+                    className="data-icon"
+                    size="20px"
                     style={{
-                      position: 'absolute',
+                      position: "absolute",
                       // top: '-3px',
-                      cursor: 'pointer'
+                      cursor: "pointer",
                     }}
                   />
                 </span>
               </th>
-              <th className='data-headings'>
+              <th className="data-headings">
                 Email
-                <span className='data-heading-icon'>
+                <span className="data-heading-icon">
                   <BiFilter
                     onClick={filterHandler}
-                    className='data-icon'
-                    size='20px'
+                    className="data-icon"
+                    size="20px"
                     style={{
-                      position: 'absolute',
+                      position: "absolute",
                       // top: '-3px',
-                      cursor: 'pointer'
+                      cursor: "pointer",
                     }}
                   />
                 </span>
               </th>
-              <th className='data-headings'>
+              <th className="data-headings">
                 Phone Number
-                <span className='data-heading-icon'>
+                <span className="data-heading-icon">
                   <BiFilter
                     onClick={filterHandler}
-                    className='data-icon'
-                    size='20px'
+                    className="data-icon"
+                    size="20px"
                     style={{
-                      position: 'absolute',
+                      position: "absolute",
                       // top: '0px',
-                      cursor: 'pointer'
+                      cursor: "pointer",
                     }}
                   />
                 </span>
               </th>
-              <th className='data-headings'>
+              <th className="data-headings">
                 Date Joined
-                <span className='data-heading-icon'>
+                <span className="data-heading-icon">
                   <BiFilter
                     onClick={filterHandler}
-                    className='data-icon'
-                    size='20px'
+                    className="data-icon"
+                    size="20px"
                     style={{
-                      position: 'absolute',
+                      position: "absolute",
                       // top: '-3px',
-                      cursor: 'pointer'
+                      cursor: "pointer",
                     }}
                   />
                 </span>
               </th>
-              <th className='data-headings'>
+              <th className="data-headings">
                 Status
-                <span className='data-heading-icon'>
+                <span className="data-heading-icon">
                   <BiFilter
                     onClick={filterHandler}
-                    className='data-icon'
-                    size='20px'
+                    className="data-icon"
+                    size="20px"
                     style={{
-                      position: 'absolute',
+                      position: "absolute",
                       // top: '-3px',
-                      cursor: 'pointer'
+                      cursor: "pointer",
                     }}
                   />
                 </span>
@@ -149,23 +149,23 @@ const UserData = () => {
           </thead>
           <tbody>
             {data ? (
-              data.slice(0, 10).map(item => (
-                <tr className='user-data-item-container' key={item.id}>
-                  <td className='user-data-item'>{item.orgName}</td>
-                  <td className='user-data-item'>{item.userName}</td>
-                  <td className='user-data-item'>{item.email}</td>
-                  <td className='user-data-item'>{item.phoneNumber}</td>
-                  <td className='user-data-item'>
-                    {dateFormat(`${item.createdAt}`, 'mmm dd, yyyy hh:M TT')}
+              data.slice(0, 10).map((item) => (
+                <tr className="user-data-item-container" key={item.id}>
+                  <td className="user-data-item">{item.orgName}</td>
+                  <td className="user-data-item">{item.userName}</td>
+                  <td className="user-data-item">{item.email}</td>
+                  <td className="user-data-item">{item.phoneNumber}</td>
+                  <td className="user-data-item">
+                    {dateFormat(`${item.createdAt}`, "mmm dd, yyyy hh:M TT")}
                   </td>
-                  <td className='user-data-item user-data-status-item'>
-                    <div className='status pending'>Pending</div>
-                    <div className='status-icon'>
+                  <td className="user-data-item user-data-status-item">
+                    <div className="status pending">Pending</div>
+                    <div className="status-icon">
                       <RxDotsVertical
                         onClick={statusHandler}
-                        size='20px'
+                        size="20px"
                         style={{
-                          cursor: 'pointer'
+                          cursor: "pointer",
                         }}
                       />
                     </div>
@@ -173,8 +173,8 @@ const UserData = () => {
                 </tr>
               ))
             ) : (
-              <tr className='user-data-error-container'>
-                <td className='user-data-error'>Loading...</td>
+              <tr className="user-data-error-container">
+                <td className="user-data-error">Loading...</td>
               </tr>
             )}
           </tbody>
@@ -182,7 +182,7 @@ const UserData = () => {
       </div>
       <Pagination />
     </>
-  )
-}
+  );
+};
 
-export default UserData
+export default UserData;
--- src/routes/userDetails/userDetails.components.jsx
+++ src/routes/userDetails/userDetails.components.jsx
@@ -1,15 +1,15 @@
-import SideBar from '../../components/sideBar/sidebar.component'
-import Navigation from '../../components/navigation/navigation.component'
-import UserProfile from '../../components/userProfileDetails/userProfile.component'
-import LogOut from '../../components/logout/logout.component'
+import SideBar from "../../components/sideBar/sidebar.component";
+import Navigation from "../../components/navigation/navigation.component";
+import UserProfile from "../../components/userProfileDetails/userProfile.component";
+import LogOut from "../../components/logout/logout.component";
 
-import './userDetails.styles.scss'
+import "./userDetails.styles.scss";
 
 const UserDetails = () => {
   return (
-    <div className='user-details-container'>
+    <div className="user-details-container">
       <Navigation />
-      <div className='details-container'>
+      <div className="details-container">
         <div>
           <SideBar />
           <LogOut />
@@ -19,7 +19,7 @@ const UserDetails = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default UserDetails
+export default UserDetails;
